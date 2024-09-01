8fe54d3ff5c5796bd0c27bd5757eb4912bd24222
elkomysara <sa1244@fayoum.edu.eg>
2024-09-01 10:21:17 +0300

    Completed Task 9: 9-easter_egg.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join([chr(c) if c % 2 == 0 else chr(c - 32) for c in range(122, 96, -1)]), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
