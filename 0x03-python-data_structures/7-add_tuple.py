d95a870e8d9a030cca28c8829858d80fa4bed327
elkomysara <sa1244@fayoum.edu.eg>
2024-09-01 20:40:50 +0300

    Completed Task 7: 7-edges.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least two elements, using 0 if not
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    
+    # Add the first and second elements of both tuples
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    
+    return new_tuple
