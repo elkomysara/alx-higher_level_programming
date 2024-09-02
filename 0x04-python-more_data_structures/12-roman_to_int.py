9514029d352ea85a7558018383276c37457e5e87
elkomysara <sa1244@fayoum.edu.eg>
2024-09-02 07:24:10 +0300

    Completed Task 9: 9-easter_egg.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-
     roman_values = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 
         'C': 100, 'D': 500, 'M': 1000
