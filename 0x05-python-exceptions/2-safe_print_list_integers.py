102ccdf4100fa5ca6bb8e741fb4771ea0f9386c2
elkomysara <sa1244@fayoum.edu.eg>
2024-09-07 23:10:57 +0300

    Completed Task 2: 2-unq_add

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,14 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     output = ""
-    for i in range(x):
-        try:
-            output += "{:d}".format(my_list[i])
-            count += 1
-        except (ValueError, TypeError):
-            continue
+    try:
+        for i in range(x):
+            try:
+                output += "{:d}".format(my_list[i])
+                count += 1
+            except (ValueError, TypeError):
+                continue
+    except IndexError:
+        pass
     print(output)
     return count
