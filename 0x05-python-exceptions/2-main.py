2cee45b060f3c7be18370295bd8f0787272416a0
elkomysara <sa1244@fayoum.edu.eg>
2024-09-07 22:42:11 +0300

    Completed Task 2: 2-unq_add

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -9,6 +9,5 @@ print("nb_print: {:d}".format(nb_print))
 my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
 nb_print = safe_print_list_integers(my_list, 7)
 print("nb_print: {:d}".format(nb_print))
-
 nb_print = safe_print_list_integers(my_list, 9)
 print("nb_print: {:d}".format(nb_print))
