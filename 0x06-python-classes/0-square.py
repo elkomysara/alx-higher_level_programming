ddfe4e82599d16c4773ea1c6311baae0aa86f619
elkomysara <sa1244@fayoum.edu.eg>
2024-09-08 22:30:46 +0300

    Completed Task 0: 0-square with proper documentation and permissions

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-This module defines an empty class `Square`.
-"""
+""" This module defines an empty class `Square`."""
 class Square:
-    """
-    This is an empty class that defines a square.
-    """
+    """ This is an empty class that defines a square. """
     pass
