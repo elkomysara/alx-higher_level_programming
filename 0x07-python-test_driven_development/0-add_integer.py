08d96e9cd4b10467549273640f38c8cda784d30d
elkomysara <sa1244@fayoum.edu.eg>
2024-09-09 23:40:31 +0300

    Fixed add_integer function and tests with proper docstring lengths.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,23 +4,12 @@ This is the "0-add_integer" module.
 The module supplies one function, add_integer(a, b).
 """
 
-
 def add_integer(a, b=98):
     """
-    Returns the addition of two integers or floats casted to integers.
-
-    Args:
-        a: The first number to add (integer or float).
-        b: The second number to add (integer or float, defaults to 98).
-
-    Returns:
-        The sum of a and b as an integer.
-
-    Raises:
-        TypeError: If either a or b are not integers or floats.
+    Return the sum of two integers or floats as integers.
     """
-    if type(a) not in [int, float]:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
