f7cb1fd5890a0f4c96af77d64b522f51ff0f7d3c
elkomysara <sa1244@fayoum.edu.eg>
2024-09-15 00:56:43 +0300

    Completed Task 9: N Queens problem solver implemented.

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+N Queens Puzzle solver
+"""
+
+import sys
+
+
+def is_safe(board, row, col):
+    """Check if a queen can be placed on the board[row][col]."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens(board, col):
+    """Use backtracking to solve the N Queens problem."""
+    if col >= len(board):
+        print_solution(board)
+        return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens(board, col + 1) or res
+            board[i][col] = 0
+    return res
+
+
+def print_solution(board):
+    """Print the solution in the required format."""
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * N for _ in range(N)]
+    solve_nqueens(board, 0)
