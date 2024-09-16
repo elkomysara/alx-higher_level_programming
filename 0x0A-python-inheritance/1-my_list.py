522e79c19a198bc8c31d2f8589b44af87448efa8
elkomysara <sa1244@fayoum.edu.eg>
2024-09-16 06:52:28 +0300

    Completed Task 1: MyList class with print_sorted method and added tests.

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
 Module 1-my_list
-Contains class MyList that inherits from list
+Defines a class MyList that inherits from list.
 """
 
 
 class MyList(list):
-    """Inherits from list and has a method to print sorted list"""
+    """
+    A subclass of list with a method to print the list in sorted order.
+    """
 
     def print_sorted(self):
-        """Prints the list, sorted in ascending order"""
+        """
+        Prints the list in ascending sorted order.
+        """
         print(sorted(self))
-
-
