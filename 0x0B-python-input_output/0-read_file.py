a89b0a37fadd5bbeabc5c6729bb2363fb7b13d9b
elkomysara <sa1244@fayoum.edu.eg>
2024-09-17 12:15:46 +0300

    Completed Task 0: Read file and print contents.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function for reading a file."""
+
+
 def read_file(filename=""):
-    """Read a UTF8 text file and print it to stdout."""
-    with open(filename, encoding="utf-8") as f:
+    """Read a text file (UTF8) and print it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
