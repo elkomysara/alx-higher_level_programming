aeec769f77c1f8986a48bcac1acbc8bf1a18b8e2
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 12:46:09 +0300

    Completed Task 11: Added size getter and setter for Square.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,17 @@ class Square(Rectangle):
         """Initialize a new Square instance."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square, applying it to both width and height."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Override the __str__ method to return [Square] (<id>) <x>/<y> - <size>."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
