76683a4dc5a423b67310366d5a68f527dbe3531c
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 13:19:29 +0300

    Completed Task 15: Implemented to_json_string method in Base class.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-"""Defines a base model class."""
+"""Defines a base class for all other classes in this project."""
+import json
+
 
 class Base:
-    """Base model.
+    """Represents the base model.
 
-    Represents the "base" of all other classes in the project.
-
-    Attributes:
-        __nb_objects (int): The number of instantiated Bases.
+    This class will be the “base” of all other classes in this project.
+    Manages the id attribute to avoid duplicating the same code.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
-        """
+        """Initialize a new Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
