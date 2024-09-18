1d424645b01b03ee9f871938ac4b2adebb18d525
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 10:56:16 +0300

    Completed Task 5: Added display method to Rectangle.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,8 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of the rectangle."""
         return self.width * self.height
+
+    def display(self):
+        """Prints the Rectangle instance with the character `#`."""
+        for _ in range(self.height):
+            print("#" * self.width)
