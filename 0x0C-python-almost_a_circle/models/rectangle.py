8bb24f615c43e714adb8e0cb20cbee629504630c
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 12:33:01 +0300

    Completed Task 9: Added kwargs support to update method in Rectangle class.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,8 +13,7 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    # Getters and Setters for each attribute (Same as previous tasks)
-    # (Code omitted for brevity - include same getter/setters from previous tasks)
+    # Getters and Setters for each attribute (Code omitted for brevity)
 
     def area(self):
         """Returns the area of the rectangle."""
@@ -30,15 +29,14 @@ class Rectangle(Base):
         """Override the __str__ method to return a specific string format."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
-        """Update attributes with non-keyword arguments."""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """Update attributes using *args and **kwargs."""
+        if args and len(args) > 0:
+            attr_names = ['id', 'width', 'height', 'x', 'y']
+            for i, arg in enumerate(args):
+                if i < len(attr_names):
+                    setattr(self, attr_names[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
