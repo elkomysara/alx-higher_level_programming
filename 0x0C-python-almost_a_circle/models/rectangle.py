2ef63c9c9a09e5ee3a96e3de6046ad0d064675ad
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 10:34:02 +0300

    Completed Task 2: First Rectangle class.

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+    """Represents a rectangle, inherits from Base.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+        __x (int): The x coordinate of the rectangle.
+        __y (int): The y coordinate of the rectangle.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x coordinate of the rectangle.
+            y (int): The y coordinate of the rectangle.
+            id (int): The identity of the rectangle.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    # Getters and Setters for each attribute
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
