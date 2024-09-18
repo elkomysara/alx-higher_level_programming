3ee44ef948e006473b361fdd45165a0599a198ec
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 13:29:57 +0300

    Completed Task 17: Implemented from_json_string method in Base class.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,25 +22,28 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries.
-
-        Args:
-            list_dictionaries (list): A list of dictionaries.
-        
-        Returns:
-            str: JSON string representation of list_dictionaries.
-        """
+        """Return the JSON string representation of list_dictionaries."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Write the JSON string representation of list_objs to a file.
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation.
 
         Args:
-            list_objs (list): A list of instances that inherit from Base (Rectangle or Square).
+            json_string (str): A string representing a list of dictionaries.
+        
+        Returns:
+            list: A list represented by json_string.
         """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
         filename = cls.__name__ + ".json"
         list_dicts = [obj.to_dictionary() for obj in list_objs] if list_objs else []
         with open(filename, "w", encoding="utf-8") as f:
