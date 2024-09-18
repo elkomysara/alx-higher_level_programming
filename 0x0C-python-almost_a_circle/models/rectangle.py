ed4fca02e1f07493958b8c5600ff3b667847ad33
elkomysara <sa1244@fayoum.edu.eg>
2024-09-18 13:11:01 +0300

    Completed Task 13: Implemented to_dictionary method for Rectangle.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,37 +6,105 @@ class Rectangle(Base):
     """Represents a rectangle, inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle."""
+        """Initialize a new Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    # Getters and Setters for each attribute (Code omitted for brevity)
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x value of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x value of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y value of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y value of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
     def area(self):
-        """Returns the area of the rectangle."""
+        """Return the area of the rectangle."""
         return self.width * self.height
 
     def display(self):
-        """Prints the Rectangle instance with the character `#` taking care of x and y."""
+        """Print the rectangle instance using the # character."""
         print("\n" * self.y, end="")
         for _ in range(self.height):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        """Override the __str__ method to return a specific string format."""
+        """Return the string representation of the rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
-        """Update attributes using *args and **kwargs."""
+        """Update attributes of the rectangle."""
         if args and len(args) > 0:
-            attr_names = ['id', 'width', 'height', 'x', 'y']
-            for i, arg in enumerate(args):
-                if i < len(attr_names):
-                    setattr(self, attr_names[i], arg)
-        elif kwargs:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+        if not args:
             for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the rectangle."""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
