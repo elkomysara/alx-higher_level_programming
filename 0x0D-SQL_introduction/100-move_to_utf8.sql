4bc9b14d138c5d601b98c0f1873897a5bb1e517a
elkomysara <sa1244@fayoum.edu.eg>
2024-09-24 14:00:13 +0300

    Add 100-move_to_utf8.sql for task 17: Convert database, table, and field to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,10 @@
+-- Script to convert database, table, and field to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+-- Convert database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert specific field (name) to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
