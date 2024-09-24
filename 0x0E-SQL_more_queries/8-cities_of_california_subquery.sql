a1bdae1af98f42bcda1d7dfdca34358ae3dc9f47
elkomysara <sa1244@fayoum.edu.eg>
2024-09-24 20:25:01 +0300

    Add 8-cities_of_california_subquery.sql to list cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- List all cities of California
+SELECT cities.id, cities.name 
+FROM cities
+WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California')
+ORDER BY cities.id ASC;
