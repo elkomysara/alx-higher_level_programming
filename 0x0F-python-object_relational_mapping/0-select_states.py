750043593d7626080b48ae8cd6fe2fa6aa2cce3f
elkomysara <sa1244@fayoum.edu.eg>
2024-09-27 09:31:00 +0300

    Add 0-select_states.py and 0-select_states.sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa.
-Arguments:
-    mysql username
-    mysql password
-    database name
+Module that connects a python script to a database
 """
 
-import MySQLdb
-import sys
-
 if __name__ == "__main__":
-    # Connect to the database
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+
+    import MySQLdb
+    from sys import argv
+
+    # Connect database using command-line arguments
+    my_database = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
     )
-    # Create a cursor object
-    cursor = db.cursor()
-    # Execute SQL query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    # Fetch all results
-    states = cursor.fetchall()
-    # Print each state
-    for state in states:
-        print(state)
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    # define cursor used to execute mysql queries
+    my_cursor = my_database.cursor()
+
+    # Execute a SELECT query to select data
+    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
+
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
+
+    # Close all cursors
+    my_cursor.close()
+
+    # Close all databases
+    my_database.close()
